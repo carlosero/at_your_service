0fdb3b25e74a9d06f82841ca8e6f068aef25a72c
Carlos Rodríguez <carlosr706@gmail.com>
2016-02-23 23:54:16 -0500

    UI fixes

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     waiter
     customer
     association :table, factory: :table_in_use
-    status :active
+    status 'active'
     factory :order_with_foods do
       transient do
         foods_count 5
