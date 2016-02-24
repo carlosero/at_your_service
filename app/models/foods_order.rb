8aaeac5807b704d6c55886504be3c3a2a46839fc
Carlos Rodríguez <carlosr706@gmail.com>
2016-02-24 11:11:22 -0500

    Small price fix

--- app/models/foods_order.rb
+++ app/models/foods_order.rb
@@ -2,6 +2,8 @@ class FoodsOrder < ActiveRecord::Base
   belongs_to :food
   belongs_to :order
 
+  before_save { |foods_order| foods_order.amount = 1 unless foods_order.amount }
+
   def total_price
     food.price * amount
   end
